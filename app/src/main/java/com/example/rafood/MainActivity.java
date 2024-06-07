215cf1e078ce7a556280644a69a23b2f3c27277c
unknown <berlianafebriani7@gmail.com>
2024-06-07 12:19:20 +0700

    changes title

--- app/src/main/java/com/example/rafood/MainActivity.java
+++ app/src/main/java/com/example/rafood/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.rafood;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        fragment(new ListProduk());
+    }
+
+    // fungsi untuk pindah halaman
+    private void fragment (Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.mainFrame, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
